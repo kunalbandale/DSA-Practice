83246f563437821a31ca84f277d309f905cc2e69
kunalbandale <bandalekunal@gmail.com>
2025-01-30 16:34:51 +0530

    add files

---
+++ 03_patterns/nested_for_loop.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace  std;
+
+int main() {
+  for(int i = 1; i <= 3; i++) {
+    cout << i << endl;
+    for(int j=1;j<=2;j++) {
+      cout << "  " <<  j ;;
+    }
+    cout << endl;
+  }
+  return 0;
+}
