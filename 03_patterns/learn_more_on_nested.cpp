d63a8f219df8948cc722e4774c1c85d9771fe18d
kunalbandale <bandalekunal@gmail.com>
2025-02-01 18:40:38 +0530

    add code files

---
+++ 03_patterns/learn_more_on_nested.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace  std;
+
+int main() {
+  for(int i = 1 ; i<=3;i++) {
+    cout << i << "->" ;
+    for(int j = 1; j<=3;j++) {
+      cout << "*" ;
+    }
+    cout << endl;
+    }
+
+    return 0;
+}
